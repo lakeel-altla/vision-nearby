256728b6a4f2582d360b57cb87a0c43d9118e8f5
sunada-ke <sunada-ke@legendapl.com>
2016-11-17 11:28:41 +0900

    [REFACTOR] Remove an unused method

--- app/src/main/java/com/lakeel/profile/notification/data/mapper/RecentlyEntityMapper.java
+++ app/src/main/java/com/lakeel/profile/notification/data/mapper/RecentlyEntityMapper.java
@@ -56,32 +56,4 @@ public final class RecentlyEntityMapper {
         }
         return entity;
     }
-
-
-    public RecentlyEntity map(String id, DetectedActivity detectedActivity, Location location, Weather weather) {
-        RecentlyEntity entity = new RecentlyEntity();
-        entity.id = id;
-        if (detectedActivity != null) {
-            entity.userActivity = detectedActivity.getType();
-        }
-        if (weather != null) {
-            entity.weather = new WeatherEntity();
-            entity.weather.humidity = weather.getHumidity();
-            entity.weather.temperature = weather.getTemperature(Weather.CELSIUS);
-
-            List<Integer> conditions = new ArrayList<>(weather.getConditions().length);
-            for (int condition : weather.getConditions()) {
-                conditions.add(condition);
-            }
-            entity.weather.conditions = conditions;
-        }
-
-        if (location != null) {
-            LocationEntity locationEntity = new LocationEntity();
-            locationEntity.latitude = String.valueOf(location.getLatitude());
-            locationEntity.longitude = String.valueOf(location.getLongitude());
-            entity.location = locationEntity;
-        }
-        return entity;
-    }
 }
