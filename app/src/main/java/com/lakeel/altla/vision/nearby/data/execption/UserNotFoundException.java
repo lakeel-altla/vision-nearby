94c117f5ede158ab633d0afba426947208d82f50
sunada-ke <sunada-ke@legendapl.com>
2016-12-02 14:38:07 +0900

    [DOC] Remove unused classes

--- app/src/main/java/com/lakeel/altla/vision/nearby/data/execption/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.lakeel.altla.vision.nearby.data.execption;
-
-public class UserNotFoundException extends RuntimeException {
-
-    public UserNotFoundException() {
-    }
-}
--- app/src/main/java/com/lakeel/altla/vision/nearby/data/rest/cm/api/CmApi.java
+++
@@ -1,8 +0,0 @@
-package com.lakeel.altla.vision.nearby.data.rest.cm.api;
-
-public interface CmApi {
-
-//    @Headers({"Content-Type: application/json"})
-//    @POST
-//    Observable<Timestamp> saveFavorites(@Url String url, @Header("X-LaKeel-Api-Key") String apiKey, @Header("X-LaKeel-Signature") String signature, @Header("X-LaKeel-Timestamp") String timestamp, @Body CmFavoritesPostData data);
-}
