b261ed24a1a6ac44c3e9b0d6cbb0e42888261237
sunada-ke <sunada-ke@legendapl.com>
2016-12-08 20:27:20 +0900

    [REFACTOR] Add  a  'ColorRes' annotation to method parameters

--- app/src/main/java/com/lakeel/altla/vision/nearby/presentation/view/actionBar/ToolBarColor.java
+++ app/src/main/java/com/lakeel/altla/vision/nearby/presentation/view/actionBar/ToolBarColor.java
@@ -2,6 +2,7 @@ package com.lakeel.altla.vision.nearby.presentation.view.actionBar;
 
 import android.app.Activity;
 import android.graphics.drawable.ColorDrawable;
+import android.support.annotation.ColorRes;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -14,7 +15,7 @@ public final class ToolBarColor implements BarColor {
 
     private final int primaryDarkColorInt;
 
-    public ToolBarColor(Activity activity, int primaryColorInt, int primaryDarkColorInt) {
+    public ToolBarColor(Activity activity, @ColorRes int primaryColorInt, @ColorRes int primaryDarkColorInt) {
         this.activity = activity;
         this.primaryColorInt = primaryColorInt;
         this.primaryDarkColorInt = primaryDarkColorInt;
