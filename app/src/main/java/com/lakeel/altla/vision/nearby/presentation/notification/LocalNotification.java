6ad243607e6143128e3122840b09e579fd403cd0
sunada-ke <sunada-ke@legendapl.com>
2016-12-08 20:58:38 +0900

    [DOC] Remove an unused import declaration

--- app/src/main/java/com/lakeel/altla/vision/nearby/presentation/notification/LocalNotification.java
+++ app/src/main/java/com/lakeel/altla/vision/nearby/presentation/notification/LocalNotification.java
@@ -4,7 +4,6 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
-import android.support.annotation.StringRes;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.ContextCompat;
 
