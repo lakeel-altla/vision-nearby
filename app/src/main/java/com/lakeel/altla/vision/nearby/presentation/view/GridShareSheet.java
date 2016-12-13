0e2ba2c8de0b87720a03b42e262704fc979bddb3
sunada-ke <sunada-ke@legendapl.com>
2016-12-13 14:28:01 +0900

    [REFACTOR] Remove m prefix of the variables

--- app/src/main/java/com/lakeel/altla/vision/nearby/presentation/view/GridShareSheet.java
+++ app/src/main/java/com/lakeel/altla/vision/nearby/presentation/view/GridShareSheet.java
@@ -1,51 +1,51 @@
 package com.lakeel.altla.vision.nearby.presentation.view;
 
-import com.flipboard.bottomsheet.BottomSheetLayout;
-import com.flipboard.bottomsheet.commons.MenuSheetView;
-
 import android.content.Context;
 import android.support.annotation.MenuRes;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.flipboard.bottomsheet.BottomSheetLayout;
+import com.flipboard.bottomsheet.commons.MenuSheetView;
+
 public final class GridShareSheet {
 
     private static final String MENU_TITLE_SHARE = "Share";
 
-    private BottomSheetLayout mBottomSheetLayout;
+    private BottomSheetLayout bottomSheetLayout;
 
-    private MenuSheetView mMenuSheetView;
+    private MenuSheetView menuSheetView;
 
-    private MenuSheetView.OnMenuItemClickListener mOnMenuItemClickListener;
+    private MenuSheetView.OnMenuItemClickListener onMenuItemClickListener;
 
     public GridShareSheet(Context context, BottomSheetLayout bottomSheetLayout, @MenuRes int menuRes) {
-        mBottomSheetLayout = bottomSheetLayout;
+        this.bottomSheetLayout = bottomSheetLayout;
 
-        mMenuSheetView =
+        menuSheetView =
                 new MenuSheetView(context, MenuSheetView.MenuType.GRID, MENU_TITLE_SHARE, item -> {
-                    if (mBottomSheetLayout.isSheetShowing()) {
-                        mBottomSheetLayout.dismissSheet();
+                    if (this.bottomSheetLayout.isSheetShowing()) {
+                        this.bottomSheetLayout.dismissSheet();
                     }
-                    mOnMenuItemClickListener.onMenuItemClick(item);
+                    onMenuItemClickListener.onMenuItemClick(item);
                     return true;
                 });
-        mMenuSheetView.inflateMenu(menuRes);
+        menuSheetView.inflateMenu(menuRes);
     }
 
     public void setOnMenuItemClickListener(MenuSheetView.OnMenuItemClickListener listener) {
-        mOnMenuItemClickListener = listener;
+        onMenuItemClickListener = listener;
     }
 
     public void hideMenuItem(int itemId) {
-        Menu menu = mMenuSheetView.getMenu();
+        Menu menu = menuSheetView.getMenu();
         MenuItem item = menu.findItem(itemId);
         if (item != null) {
             item.setVisible(false);
-            mMenuSheetView.updateMenu();
+            menuSheetView.updateMenu();
         }
     }
 
     public void show() {
-        mBottomSheetLayout.showWithSheetView(mMenuSheetView);
+        bottomSheetLayout.showWithSheetView(menuSheetView);
     }
 }
