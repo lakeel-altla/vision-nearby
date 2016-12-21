20aa17b43879f08f4668136e027596cb25ec794f
sunada-ke <sunada-ke@legendapl.com>
2016-12-21 17:49:55 +0900

    [DOC] Add a class for check the arguments

---
+++ app/src/main/java/com/lakeel/altla/vision/nearby/core/NullArgumentException.java
@@ -0,0 +1,8 @@
+package com.lakeel.altla.vision.nearby.core;
+
+public final class NullArgumentException extends RuntimeException {
+
+    public NullArgumentException(String arg) {
+        super(arg + "is null");
+    }
+}
