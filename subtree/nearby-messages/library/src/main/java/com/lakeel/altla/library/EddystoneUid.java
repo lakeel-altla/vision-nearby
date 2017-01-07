bb09d3b1bcb61e4a3b7739f28a14869653784f22
sunada-ke <sunada-ke@legendapl.com>
2017-01-08 04:38:05 +0900

    [REFACTOR] Remove 'm' prefix of a variable

--- subtree/nearby-messages/library/src/main/java/com/lakeel/altla/library/EddystoneUid.java
+++ subtree/nearby-messages/library/src/main/java/com/lakeel/altla/library/EddystoneUid.java
@@ -8,7 +8,7 @@ public final class EddystoneUID {
     /**
      * The 16-byte ID, as a hex string.
      */
-    private final String mBeaconId;
+    private final String beaconId;
 
     /**
      * Constructs an EddystoneUID with the 16-byte ID, as a hex string.
@@ -18,20 +18,20 @@ public final class EddystoneUID {
      */
     public EddystoneUID(String beaconId) {
         if (beaconId == null) throw new IllegalArgumentNullException("beaconId");
-        mBeaconId = beaconId;
+        this.beaconId = beaconId;
     }
 
     /**
      * @return The namespace ID (first 10 bytes), as a hex string.
      */
     public String getNamespaceId() {
-        return mBeaconId.substring(0, 20);
+        return beaconId.substring(0, 20);
     }
 
     /**
      * @return The instance ID (last 6 bytes), as a hex string.
      */
     public String getInstanceId() {
-        return mBeaconId.substring(20, 32);
+        return beaconId.substring(20, 32);
     }
 }
