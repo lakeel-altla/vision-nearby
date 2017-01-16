41d93392db4c1209db7cd2fd7cec95dd2d3a362d
sunada-ke <sunada-ke@legendapl.com>
2017-01-16 20:39:46 +0900

    [REFACTOR] Remove unused classes

--- app/src/main/java/com/lakeel/altla/vision/nearby/presentation/attachment/Attachment.java
+++
@@ -1,8 +0,0 @@
-package com.lakeel.altla.vision.nearby.presentation.attachment;
-
-import android.content.Context;
-
-public interface Attachment {
-
-    void startService(Context context, String value);
-}
