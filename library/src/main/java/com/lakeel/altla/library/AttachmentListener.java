ca8778cd8b9292d95ef3480bb89b5f7840164b04
sunada-ke <sunada-ke@legendapl.com>
2017-01-16 17:21:14 +0900

    Squashed 'subtree/nearby-messages/' changes from 37be320..d6a6d60
    d6a6d60 [DOC] Add Javadoc
    346d516 [DOC] Add Javadoc
    e093c6a [REFACTOR] Add classes to nearby-messages
    
    git-subtree-dir: subtree/nearby-messages
    git-subtree-split: d6a6d601cb92d67806ef8b36237a123840057e07

--- library/src/main/java/com/lakeel/altla/library/AttachmentListener.java
+++ library/src/main/java/com/lakeel/altla/library/AttachmentListener.java
@@ -3,8 +3,14 @@ package com.lakeel.altla.library;
 import com.google.android.gms.nearby.messages.Message;
 import com.google.android.gms.nearby.messages.MessageListener;
 
+/**
+ * A abstract class for receiving subscribed messages. This callback will be delivered when messages are found.
+ */
 public abstract class AttachmentListener extends MessageListener {
 
+    /**
+     * ${inheritDoc}
+     */
     @Override
     public final void onFound(Message message) {
         String type = message.getType();
@@ -12,5 +18,11 @@ public abstract class AttachmentListener extends MessageListener {
         onFound(type, value);
     }
 
+    /**
+     * Called with attachments data.
+     *
+     * @param type The attachment of type.
+     * @param value The attachment of value.
+     */
     protected abstract void onFound(String type, String value);
 }
--- library/src/main/java/com/lakeel/altla/library/ResolutionResultCallback.java
+++ library/src/main/java/com/lakeel/altla/library/ResolutionResultCallback.java
@@ -1,12 +1,21 @@
 package com.lakeel.altla.library;
 
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Result;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
 
 import android.support.annotation.NonNull;
 
+/**
+ * An abstract class for receiving a {@link Result} from a {@link PendingResult} as an asynchronous
+ * callback.
+ */
 public abstract class ResolutionResultCallback implements ResultCallback<Status> {
 
+    /**
+     * ${inheritDoc}
+     */
     @Override
     public final void onResult(@NonNull Status status) {
         if (!status.isSuccess() && status.hasResolution()) {
@@ -14,5 +23,10 @@ public abstract class ResolutionResultCallback implements ResultCallback<Status>
         }
     }
 
+    /**
+     * Called when can resolution.
+     *
+     * @param status The results of work.
+     */
     protected abstract void onResolution(Status status);
 }
