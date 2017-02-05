aa0df9df8b9deb90a31727e1d4973e9fd802733d
sunada-ke <sunada-ke@legendapl.com>
2017-02-06 01:10:14 +0900

    [REFACTOR] Review the string resources

--- app/src/main/java/com/lakeel/altla/vision/nearby/presentation/awareness/UserActivity.java
+++ app/src/main/java/com/lakeel/altla/vision/nearby/presentation/awareness/UserActivity.java
@@ -3,14 +3,14 @@ package com.lakeel.altla.vision.nearby.presentation.awareness;
 import com.lakeel.altla.vision.nearby.R;
 
 public enum UserActivity {
-    IN_VEHICLE(com.google.android.gms.location.DetectedActivity.IN_VEHICLE, R.string.detected_activity_vehicle),
-    ON_BICYCLE(com.google.android.gms.location.DetectedActivity.ON_BICYCLE, R.string.detected_activity_bicycle),
-    ON_FOOT(com.google.android.gms.location.DetectedActivity.ON_FOOT, R.string.detected_activity_foot),
-    RUNNING(com.google.android.gms.location.DetectedActivity.RUNNING, R.string.detected_activity_running),
-    STILL(com.google.android.gms.location.DetectedActivity.STILL, R.string.detected_activity_still),
-    TILTING(com.google.android.gms.location.DetectedActivity.TILTING, R.string.detected_activity_tilting),
-    UNKNOWN(com.google.android.gms.location.DetectedActivity.UNKNOWN, R.string.detected_activity_unknown),
-    WALKING(com.google.android.gms.location.DetectedActivity.WALKING, R.string.detected_activity_walking);
+    IN_VEHICLE(com.google.android.gms.location.DetectedActivity.IN_VEHICLE, R.string.user_activity_vehicle),
+    ON_BICYCLE(com.google.android.gms.location.DetectedActivity.ON_BICYCLE, R.string.user_activity_bicycle),
+    ON_FOOT(com.google.android.gms.location.DetectedActivity.ON_FOOT, R.string.user_activity_foot),
+    RUNNING(com.google.android.gms.location.DetectedActivity.RUNNING, R.string.user_activity_running),
+    STILL(com.google.android.gms.location.DetectedActivity.STILL, R.string.user_activity_still),
+    TILTING(com.google.android.gms.location.DetectedActivity.TILTING, R.string.user_activity_tilting),
+    UNKNOWN(com.google.android.gms.location.DetectedActivity.UNKNOWN, R.string.user_activity_unknown),
+    WALKING(com.google.android.gms.location.DetectedActivity.WALKING, R.string.user_activity_walking);
 
     private int intValue;
 
