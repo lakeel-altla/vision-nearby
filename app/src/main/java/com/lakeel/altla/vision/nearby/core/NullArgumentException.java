13cadb700d7e370d8808a3b91391c2be0ed83a45
sunada-ke <sunada-ke@legendapl.com>
2017-02-13 15:52:33 +0900

    [REFACTOR] Change the parent class

--- app/src/main/java/com/lakeel/altla/vision/nearby/core/NullArgumentException.java
+++ app/src/main/java/com/lakeel/altla/vision/nearby/core/NullArgumentException.java
@@ -1,6 +1,6 @@
 package com.lakeel.altla.vision.nearby.core;
 
-public final class NullArgumentException extends NullPointerException {
+public final class NullArgumentException extends IllegalArgumentException {
 
     public NullArgumentException(String arg) {
         super(arg + "is null.");
